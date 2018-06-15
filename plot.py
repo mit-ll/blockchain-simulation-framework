d062e4fa0532c4c1fc747606ac18dbbde8feab51
gbspend <George.Spendlove@ll.mit.edu>
2018-06-15 11:35:58 -0400

    plotDag with graphviz

--- plot.py
+++ plot.py
@@ -3,104 +3,30 @@ import plotly.graph_objs as go
 import plotly.tools
 import networkx as nx
 import matplotlib.pyplot as plt
+from graphviz import Digraph
 
-# == start bitcoin ==================
+
+def txName(x):
+    return str(x.id)+'; '+x.hash()[:4]
 
 
-def printChain(node, acc=None, t=0):
-    s = '  '*t+str(node.tx.id)
-    if acc and node.tx in acc:
-        s += '*'
-    print s
+def dagToDig(miner, node, d=None):
+    i = txName(node.tx)
+    if d is None:
+        d = Digraph()
+        d.graph_attr['rankdir'] = 'RL'
+    if node.tx in miner.accepted:
+        d.node(i, fillcolor='red', style='filled')
     for c in node.children:
-        printChain(c, acc, t+1)
+        ci = txName(c.tx)
+        d.edge(ci, i)
+        dagToDig(miner, c, d)
+    return d
 
 
-def getNextFork(f, used, sign):
-    """output must match sign's sign unless sign == 0"""
-    i = 1
-    while True:
-        out = f + i
-        if out not in used:
-            if sign == 0 or (sign > 0 and out > 0) or (sign < 0 and out < 0):
-                return out
-        out = f - i
-        if out not in used:
-            if sign == 0 or (sign > 0 and out > 0) or (sign < 0 and out < 0):
-                return out
-        i += 1
-
-
-def nodesToNx(miner, node, g=None, d=0, f=0, lasty=0):
-    """d is depth (x coord)
-    f is current fork # (y coord)
-    nx.node ids must be hashes (otherwise not unique)
-    """
-    i = node.tx.hash()
-    if g is None:
-        g = nx.Graph()
-        g.add_node(i)
-        g.nodes[i]['y'] = f
-        g.nodes[i]['id'] = node.tx.id
-    g.nodes[i]['x'] = d
-    forks = {}  # maps nx.node id (hash) to fork
-    myused = set([0])
-    for c in node.children:
-        ci = c.tx.hash()
-        if ci not in g:
-            g.add_node(ci)
-        g.add_edge(ci, i)
-        if c.tx in miner.accepted:
-            assert f not in forks.values()
-            forks[ci] = f
-        else:
-            newf = getNextFork(f, myused, lasty)
-            assert not ((lasty < 0 and newf > 0) or (lasty > 0 and newf < 0))
-            forks[ci] = newf
-            myused.add(newf)
-    for c in node.children:
-        ci = c.tx.hash()
-        myf = forks[ci]
-        g.nodes[ci]['y'] = myf
-        g.nodes[ci]['id'] = c.tx.id
-        nodesToNx(miner, c, g, d+1, myf, g.nodes[i]['y'])
-    return g
-
-
-def plotChain(miner):
-    v = nodesToNx(miner, miner.root)
-    p = {i: (v.nodes[i]['x'], v.nodes[i]['y']) for i in v.nodes}
-    l = {i: v.nodes[i]['id'] for i in v.nodes}
-    nx.draw_networkx(v, node_color='#ff6666', node_size=20, pos=p, labels=l)
-    plt.show()
-
-# == end bitcoin ====================
-
-# == start iota =====================
-
-
-def iotaNodesToNx(node, g=None):  # note: this doesn't work for bitcoin because of dupe ids
-    i = node.tx.id
-    if g is None:
-        g = nx.Graph()
-        if i not in g:  # only need to do this for the root
-            g.add_node(i)
-            g.nodes[i]['children'] = 1  # len(node.children)
-    for c in node.children:
-        ci = c.tx.id
-        if ci not in g:
-            g.add_node(ci)
-            g.nodes[ci]['children'] = len(c.children)
-        g.add_edge(ci, i)
-        iotaNodesToNx(c, g)
-    return g
-
-
-def plotTangle(root):
-    v = iotaNodesToNx(root)
-    simplePlot(v, {i: (i*2, v.nodes[i]['children']-1 if i % 2 else (v.nodes[i]['children']-1)*-1)for i in v.nodes})
-
-# == end iota =======================
+def plotDag(miner, fname='test.gv'):
+    d = dagToDig(miner, miner.root)
+    d.render(fname, view=True)
 
 
 def simplePlot(G, p=None):
